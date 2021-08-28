d26e4b9f8ecfe4a9e359c9a91782f231d1634247
Sayan3990 <sayan.bhatta2017@gmail.com>
2021-08-28 09:16:42 +0530

    IF ELSE DO

--- IF_ELSE_DO.f
+++ IF_ELSE_DO.f
@@ -1,8 +1,10 @@
       program IF_ELSE
       implicit none
-      integer num, I
-      DO I=1, 20
-      write(*,*) "Enter val: "
+      integer num, N, I
+      write(*,*) "Enter N: "
+      read(*,*) N
+      DO I=1, N
+      write(*,*) "Enter num: "
       read(*,*) num
       if (mod(num,2) .eq. 0)   then
           write(*,*) "Even number"
